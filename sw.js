bc5b0e7de6d9f7cf45fdfac7cec87d77a35c2914
Aditya Agan <adityaagan0312@gmail.com>
2025-04-23 16:24:12 +0530

    first5

--- sw.js
+++ sw.js
@@ -1,7 +1,38 @@
 const CACHE_NAME = "fitness-cache-v1";
 
 const urlsToCache = [
-  "/", "/index.html", "/style.css","/images/IMG1.png"
+  "/", "/index.html", "/style.css", "/images/IMG1.png", "/images/fit1.jpg",
+  "/images/fit2.jpg",
+  "/images/FIT3.jpg",
+  "/images/fit4.jpg",
+  "/images/fit5.jpg",
+  "/images/fit6.jpg",
+  "/images/fit7.jpg",
+  "/images/fit8.jpg",
+  "/images/fit9.jpg",
+  "/images/fit11.jpg",
+  "/images/image1.webp",
+  "/images/image2.webp",
+  "/images/img2.jpg",
+  "/images/img3.jpg",
+  "/about.html",
+  "/BMI.html",
+  "/buynow.html",
+  "/cart.html",
+  "/contact.html",
+  "/chat.html",
+  "/diet.html",
+  "/feedback.html",
+  "/fitness center.html",
+  "/item.html",
+  "/login.html",
+  "/product.html",
+  "/service.html",
+  "/signup.html",
+  "/social.html",
+  "/script.js",
+  "/offline.html",
+  "/faq.html"
 ];
 
 self.addEventListener("install", (event) => {
